66b08c311c840a97dfdfb79eb7b3d6ac601dff5c
yehor <tka4951@gmail.com>
2016-10-04 13:15:34 +0300

    args check

--- ArchComp1/Program.cs
+++ ArchComp1/Program.cs
@@ -8,12 +8,33 @@ namespace ArchComp1
 {
     class Program
     {
+        private static string[] keys = { "-h", "--help" };
+
         static int Main(string[] args)
         {
+            List<int> indexes = new List<int>();
+
             if (args.Length == 0)
             {
                 Console.WriteLine("App doesn't have any arguments.");
             }
+            else
+            {
+                var isKey = false;
+                foreach (var a in args)
+                {
+                    isKey = false;
+                    if (a[0] == '-') isKey = true;
+                    if (isKey)
+                    {
+                        var index = Array.IndexOf(keys, a);
+                        if (index > -1) indexes.Add(index);
+                        else Console.WriteLine("The key {0} not found!", a);
+                    }
+                }
+            }
+
+            Console.WriteLine(indexes);
 
             Console.Read();
             return 0;
